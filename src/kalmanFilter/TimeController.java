3ca94eb77ad6ab310c07c9d77fdbd8493aaea7ff
noam mantin <noam mantin@gmail.com>
2018-02-28 18:55:24 +0200

    created TimeController which supplies time differences

---
+++ src/kalmanFilter/TimeController.java
@@ -0,0 +1,27 @@
+package kalmanFilter;
+
+import java.util.function.Supplier;
+
+public class TimeController {
+
+	private Supplier<Double> relativeTimeSupplier;
+
+	private double lastTime;
+	private double currentTime;
+
+	private TimeController(Supplier<Double> relativeTimeSupplier) {
+		this.relativeTimeSupplier = relativeTimeSupplier;
+		lastTime = 0;
+		currentTime = 0;
+	}
+
+	public double getDT() {
+		double result = currentTime - lastTime;
+
+		lastTime = currentTime;
+		currentTime = relativeTimeSupplier.get();
+
+		return result;
+	}
+
+}
