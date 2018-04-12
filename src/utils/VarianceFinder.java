4d1333eadc9f7af27712493d2526181fd1273e70
tomer sitton <tomer.sitton1@gmail.com>
2018-04-12 09:45:20 +0300

    added docs to variance finder

--- src/utils/VarianceFinder.java
+++ src/utils/VarianceFinder.java
@@ -7,23 +7,32 @@ import org.apache.commons.math3.linear.RealVector;
  * this class is used to find the variance of multiple random variables, using
  * their returned values.
  * 
+ * in the RobotOrientation project, we use this class to find the variance of
+ * the sensors in order to determine the amount of reliability of each one of
+ * them. The sensors with the smallest variance are considered to be the most
+ * reliable.
+ * 
  * @author noam mantin
  */
 public class VarianceFinder {
 
-	// the sum of the variables' returned values
+	// the sum of the variables' returned values (in our case - the sum of the
+	// measurement vectors
 	private RealVector variableResultSum;
 	// the sum of the variables' returned values' squares
 	private RealVector variableResultSquareSum;
 
-	// the number of values returned
+	// the number of values returned (in our case - the number of samples taken
+	// from the sensor)
 	private int resultNumber = 0;
 
 	/**
 	 * creates a new {@link VarianceFinder} object, with a given variable number
 	 * 
 	 * @param variableNumber
-	 *            the number of random variables
+	 *            the number of random variables (or - the number of the
+	 *            different types of measurements (which can also be referred to
+	 *            as the number of sensors))
 	 */
 	public VarianceFinder(int variableNumber) {
 		variableResultSum = new ArrayRealVector(variableNumber);
@@ -35,6 +44,7 @@ public class VarianceFinder {
 	 * 
 	 * @param newResults
 	 *            a vector which should contain the variables' returned values
+	 *            (or - the measured samples vector)
 	 */
 	public void addResultVector(double... newResults) {
 		if (newResults.length == variableResultSum.getDimension()) {
@@ -51,6 +61,14 @@ public class VarianceFinder {
 			throw new IllegalArgumentException("illegal result vector size");
 	}
 
+	/**
+	 * calculates and returns the variance vector according to the results added
+	 * by {@link #addResultVector(newResults)} until this method was called.
+	 * 
+	 * @return The variance vector of the given random variables vectors (or -
+	 *         the given sensors samples' vectors)
+	 * 
+	 */
 	public RealVector getVarianceVector() {
 
 		RealVector resultSquareMeanVector = variableResultSquareSum.mapDivide(resultNumber);
