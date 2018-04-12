bd8720d410597dfdaa63ccc3d42a8f6d0d96bdde
noam mantin <noam mantin@gmail.com>
2018-04-12 12:04:25 +0300

    added documentation to ErrorFinder

--- src/errorHandler/ErrorFinder.java
+++ src/errorHandler/ErrorFinder.java
@@ -14,15 +14,37 @@ public class ErrorFinder {
 		varianceFinder = new VarianceFinder(size);
 	}
 
+	/**
+	 * adds a given measurement to the analyzation
+	 * 
+	 * @param measurement
+	 *            the measurement vector
+	 */
 	public void addMeasurement(double... measurement) {
 		biasFinder.addMeasurement(measurement);
 		varianceFinder.addResultVector(measurement);
 	}
 
+	/**
+	 * returns the bias vector of the sensors, according to a given expected vector
+	 * 
+	 * @param expectedVector
+	 *            the expected measurement vector in the current state,.
+	 * @return the bias vector of the sensors, or their average error from the
+	 *         expected measurement
+	 * @see BiasFinder
+	 */
 	public RealVector getErrorVector(RealVector expectedVector) {
 		return biasFinder.getErrorVector(expectedVector);
 	}
 
+	/**
+	 * calculates and returns the variance vector of the sensors.
+	 * 
+	 * @return a vector, containing the variance of each sensor
+	 * 
+	 * @see VarianceFinder
+	 */
 	public RealVector getVarianceVector() {
 		return varianceFinder.getVarianceVector();
 	}
