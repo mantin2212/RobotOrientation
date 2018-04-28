a60e89ff14d084d47b12cdd6db60e67454f4b66b
noam mantin <noam mantin@gmail.com>
2018-04-28 15:34:13 +0300

    fixed getDifference method in RelativeDataHandler

--- src/orientationUtils/RelativeDataHandler.java
+++ src/orientationUtils/RelativeDataHandler.java
@@ -7,19 +7,17 @@ public class RelativeDataHandler {
 	private Supplier<Double> relativeDataSupplier;
 
 	private double lastValue;
-	private double currentValue;
 
 	public RelativeDataHandler(Supplier<Double> relativeValueSupplier) {
 		this.relativeDataSupplier = relativeValueSupplier;
-		lastValue = 0;
-		currentValue = 0;
+		lastValue = relativeDataSupplier.get();
 	}
 
 	public double getLastDifference() {
-		double result = currentValue - lastValue;
+		double currentValue = relativeDataSupplier.get();
 
+		double result = currentValue - lastValue;
 		lastValue = currentValue;
-		currentValue = relativeDataSupplier.get();
 
 		return result;
 	}
