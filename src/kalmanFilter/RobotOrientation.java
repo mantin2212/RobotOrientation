ba35a499c9864a187aaa35d7c0e928937bf3c6d7
tomer sitton <tomer.sitton1@gmail.com>
2018-05-16 20:14:23 +0300

    added initialize method to RobotOrientation.java

--- src/kalmanFilter/RobotOrientation.java
+++ src/kalmanFilter/RobotOrientation.java
@@ -11,6 +11,7 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
+import errorHandler.ErrorFinder;
 import odometry.OdometryHandler;
 import orientationUtils.OrientationConstants;
 import orientationUtils.Point3D;
@@ -42,8 +43,25 @@ public class RobotOrientation {
 
 	}
 
-	public void initialize(Point3D initialPosition, Supplier<Double> getRelativeTime) {
+	// TODO - maybe find a better name for the variable getRelativeTime
+	public void initialize(ErrorFinder errorFinder, Point3D initialPosition, Supplier<Double> getRelativeTime) {
+		this.timeController = new RelativeDataSupplier(getRelativeTime);
+		this.position = initialPosition;
+		/*
+		 * handle the biases (use getBiasVector from errorFinder somewhere
+		 * somehow)
+		 */
+		// build kalman filter
+		RealVector initialStateEstimate = new ArrayRealVector(new Double[] { 0.0, 0.0, 0.0 });
+		RealMatrix processNoise = Utils.getDiagonalMatrix(errorFinder.getVarianceVector());
+		RealMatrix measurmentNoise = Utils.get0Matrix();
 
+		ProcessModel processModel = new DefaultProcessModel(OrientationConstants.KalmanFilterMatrices.A_MATRIX,
+				OrientationConstants.KalmanFilterMatrices.B_MATRIX, processNoise, initialStateEstimate, null);
+		MeasurementModel measurementModel = new DefaultMeasurementModel(
+				OrientationConstants.KalmanFilterMatrices.H_MATRIX, measurmentNoise);
+
+		movementFilter = new KalmanFilter(processModel, measurementModel);
 	}
 
 	public void update() {
